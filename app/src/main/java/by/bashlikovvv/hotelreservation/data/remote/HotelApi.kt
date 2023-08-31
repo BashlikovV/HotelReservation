3e3fae3eb6770ae5dc0bc079edec2196e14df364
bashlikovvv <volodia413314@gmail.com>
2023-09-01 00:30:10 +0300

    HotelApi added.

---
+++ app/src/main/java/by/bashlikovvv/hotelreservation/data/remote/HotelApi.kt
@@ -0,0 +1,13 @@
+package by.bashlikovvv.hotelreservation.data.remote
+
+import by.bashlikovvv.hotelreservation.data.remote.response.HotelDto
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface HotelApi {
+
+    @GET(/*hotels/{id}*/"35e0d18e-2521-4f1b-a575-f0fe366f66e3/")
+    suspend fun getHotelById(
+        /*@Path("id") id: Long*/
+    ): Response<HotelDto>
+}
