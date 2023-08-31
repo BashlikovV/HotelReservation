327f7aef0683c69f2c9d5ea4a616d1e02e50fb90
bashlikovvv <volodia413314@gmail.com>
2023-09-01 00:31:51 +0300

    RoomsApi added.

---
+++ app/src/main/java/by/bashlikovvv/hotelreservation/data/remote/RoomsApi.kt
@@ -0,0 +1,10 @@
+package by.bashlikovvv.hotelreservation.data.remote
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface RoomsApi {
+
+//    @GET("f9a38183-6f95-43aa-853a-9c83cbb05ecd/")
+//    suspend fun gerRooms():
+}
