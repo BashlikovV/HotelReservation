fdb89033ea7272bcd93503c92990ec17a75ec640
bashlikovvv <volodia413314@gmail.com>
2023-09-01 00:32:08 +0300

    Hilt support added.

--- app/src/main/java/by/bashlikovvv/hotelreservation/presentation/view/SuccessFragment.kt
+++ app/src/main/java/by/bashlikovvv/hotelreservation/presentation/view/SuccessFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import by.bashlikovvv.hotelreservation.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SuccessFragment : Fragment() {
 
     override fun onCreateView(
