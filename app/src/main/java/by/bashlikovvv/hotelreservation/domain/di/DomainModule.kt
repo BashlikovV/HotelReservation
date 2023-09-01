78fe8f70db139605c1ee8c533eaaf835453364e5
bashlikovvv <volodia413314@gmail.com>
2023-09-01 14:04:33 +0300

    usecase added.

--- app/src/main/java/by/bashlikovvv/hotelreservation/domain/di/DomainModule.kt
+++ app/src/main/java/by/bashlikovvv/hotelreservation/domain/di/DomainModule.kt
@@ -1,7 +1,9 @@
 package by.bashlikovvv.hotelreservation.domain.di
 
 import by.bashlikovvv.hotelreservation.domain.repository.IHotelRepository
+import by.bashlikovvv.hotelreservation.domain.repository.IRoomsRepository
 import by.bashlikovvv.hotelreservation.domain.usecase.GetHotelByIdUseCase
+import by.bashlikovvv.hotelreservation.domain.usecase.GetRoomsUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,5 +16,10 @@ class DomainModule {
     @Provides
     fun provideGetHotelByIdUseCase(hotelRepository: IHotelRepository): GetHotelByIdUseCase {
         return GetHotelByIdUseCase(hotelRepository)
+    }
+
+    @Provides
+    fun provideGetRoomsUseCase(roomsRepository: IRoomsRepository): GetRoomsUseCase {
+        return GetRoomsUseCase(roomsRepository)
     }
 }
