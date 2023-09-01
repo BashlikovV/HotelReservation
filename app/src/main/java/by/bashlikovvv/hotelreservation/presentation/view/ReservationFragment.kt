882eb8913f107e3f0fbf9f36f286bbd3fb2bfc07
bashlikovvv <volodia413314@gmail.com>
2023-09-01 14:02:38 +0300

    ReservationFragment added.

--- app/src/main/java/by/bashlikovvv/hotelreservation/presentation/view/ReservationFragment.kt
+++ app/src/main/java/by/bashlikovvv/hotelreservation/presentation/view/ReservationFragment.kt
@@ -5,20 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import by.bashlikovvv.hotelreservation.R
+import by.bashlikovvv.hotelreservation.databinding.FragmentReservationBinding
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ReservationFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val binding: FragmentReservationBinding by viewBinding(CreateMethod.INFLATE)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_reservation, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val roomItem = requireArguments().getBundle(ARG_ROOM)
+    }
+
+    companion object {
+
+        const val ARG_ROOM = "room"
     }
 }
