c3e3738a2c6427e6c39d6a2f204ad71d1168eaf8
bashlikovvv <volodia413314@gmail.com>
2023-09-01 14:04:50 +0300

    RoomsDto added.

--- app/src/main/java/by/bashlikovvv/hotelreservation/data/remote/response/RoomsDto.kt
+++ app/src/main/java/by/bashlikovvv/hotelreservation/data/remote/response/RoomsDto.kt
@@ -3,5 +3,5 @@ package by.bashlikovvv.hotelreservation.data.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class RoomsDto(
-    @SerializedName("rooms") val rooms: List<RoomsItem?>
+    @SerializedName("rooms") val rooms: List<RoomsItemDto?>
 )
