7a389c8a4e44a8dfadde431c089e15fe5673bb4d
bashlikovvv <volodia413314@gmail.com>
2023-09-01 14:01:06 +0300

    ViewBinding added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     //Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
+    //ViewBinding
+    implementation("com.github.kirich1409:viewbindingpropertydelegate-full:1.5.9")
     //Glide
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     // Retrofit
