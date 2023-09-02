63d9fd4889fafe00571c756ceb24dff0b5fc31c7
bashlikovvv <volodia413314@gmail.com>
2023-09-02 19:14:17 +0300

    Reservation added.

---
+++ app/src/main/java/by/bashlikovvv/hotelreservation/domain/model/Reservation.kt
@@ -0,0 +1,19 @@
+package by.bashlikovvv.hotelreservation.domain.model
+
+data class Reservation(
+    val hotelAddress: String = "",
+    val rating: Int = 0,
+    val fuelCharge: Int = 0,
+    val ratingName: String = "",
+    val tourDateStart: String = "",
+    val tourDateStop: String = "",
+    val room: String = "",
+    val hotelName: String = "",
+    val arrivalCountry: String = "",
+    val numberOfNights: Int = 0,
+    val nutrition: String = "",
+    val serviceCharge: Int = 0,
+    val id: Long = 0L,
+    val departure: String = "",
+    val tourPrice: Int = 0
+)
