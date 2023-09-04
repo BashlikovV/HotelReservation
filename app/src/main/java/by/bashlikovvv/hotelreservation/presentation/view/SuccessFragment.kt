3cfc98dcc91e9617777eb9b1dbddb2d1e9d496fb
bashlikovvv <volodia413314@gmail.com>
2023-09-04 11:17:13 +0300

    SuccessFragment added.

--- app/src/main/java/by/bashlikovvv/hotelreservation/presentation/view/SuccessFragment.kt
+++ app/src/main/java/by/bashlikovvv/hotelreservation/presentation/view/SuccessFragment.kt
@@ -6,11 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import by.bashlikovvv.hotelreservation.R
+import by.bashlikovvv.hotelreservation.databinding.FragmentSuccessBinding
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
 
 @AndroidEntryPoint
 class SuccessFragment : Fragment() {
+
+    private val binding: FragmentSuccessBinding by viewBinding(createMethod = CreateMethod.INFLATE)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +27,19 @@ class SuccessFragment : Fragment() {
             setHomeAsUpIndicator(R.drawable.navigation_icon)
             title = getString(R.string.success)
         }
-        return inflater.inflate(R.layout.fragment_success, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val orderNum = Random().nextInt(Int.MAX_VALUE)
+        binding.successText.text = getString(R.string.order_text, orderNum.toString())
+        binding.include.selectRoomBtn.apply {
+            text = getString(R.string.super_btn_text)
+            setOnClickListener {
+                findNavController().popBackStack(R.id.hotelFragment, true)
+            }
+        }
     }
 }
