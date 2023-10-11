3d2e0e10bec05f79218a6e7fc5a86903447d77fd
bashlikovvv <volodia413314@gmail.com>
2023-10-11 09:56:49 +0300

    Phone data model added.

--- domain/src/main/java/by/bashlikovvv/domain/model/Phone.kt
+++ domain/src/main/java/by/bashlikovvv/domain/model/Phone.kt
@@ -1,7 +1,3 @@
 package by.bashlikovvv.domain.model
 
-data class Phone(
-    val currentValue: String = "",
-    val currentNew: Char = ' ',
-    val newText: String = ""
-)
+data class Phone(val currentValue: String = "")
